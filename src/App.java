187e025f2b23e3ab4a081781f77ed125eca6b312
Yuepeng Xiong <yuepengxiong1@gmail.com>
2024-10-02 16:07:00 -0400

    foruth commit

--- src/App.java
+++ src/App.java
@@ -2,51 +2,66 @@
  * Yuepeng Xiong
  * 9/26/24
  * 
- * Use arguments from command line or if no arguments are inputted, ask for files. Look for text files that contains numbers and 
- * put them into in each array, then multiply each array and output a new text file
+ * Check if user input a number or 2 files. If a number, use that number to generate a 2 2d square array and
+ * populate them with random numbers and multiply them. Generate text files for 3 arrays.
  * 
- * 
+ * If user passes in 2 text files of a 2d array, accept them and do the same. Generate the third text file.
  * 
  */
+import java.io.FileWriter;
 import java.util.Scanner;
-
-
-
-
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //File file1 = new File("tester");
-        //File file2 = new File("");
+        FileWriter textFile1 = new FileWriter("matrix1.txt");
+        FileWriter textFile2 = new FileWriter("matrix2.txt");
+        FileWriter textFile3 = new FileWriter("matrix3.txt");
         Scanner scanner = new Scanner(System.in);
         int setMatrixRowColumn;
-
-
-        System.out.println(args.length);
+        int store[] = null;
+        int counter = 0;
+        
         //first checks if there are 2 aruments passed in
         if(args.length == 0){
             System.out.println("Nothing was passed in through command line. Enter a number or 2 files.");
 
         } else if (args.length == 1){
+            //get the string of the user input, convert to int
             setMatrixRowColumn = Integer.parseInt(args[0]);
-            System.out.println(setMatrixRowColumn);
+
+            //set 2d array to the dimensions of given number from command line
             int[][] array1 = new int[setMatrixRowColumn][setMatrixRowColumn];
             int[][] array2 = new int[setMatrixRowColumn][setMatrixRowColumn];
+            int[][] array3 = new int[setMatrixRowColumn][setMatrixRowColumn];
 
+            //populate 2d array for array1
             for (int i = 0; i < setMatrixRowColumn; i++){
                 for(int j = 0; j <setMatrixRowColumn; j++){
-                    array1[i][j] = (int)(Math.random() * 100);
+                    array1[i][j] = (int)(Math.random() * 100);//generates number from 1-100
+                    store[counter] = array1[i][j];
+                    counter++;
+                }
+                counter++;
+            }
+            System.out.println(counter);
+            //populate 2d array for array2
+            for (int i = 0; i < setMatrixRowColumn; i++){
+                for(int j = 0; j <setMatrixRowColumn; j++){
+                    array2[i][j] = (int)(Math.random() * 100); //generates number from 1-100
                 }
             }
 
+            //multiply both 2d arrays
             for (int i = 0; i < setMatrixRowColumn; i++){
                 for(int j = 0; j <setMatrixRowColumn; j++){
-                    System.out.println("Output of row " + i + " colomn " + j + ": " + array1[i][j]);
+                    array3[i][j] = array1[i][j] * array2[i][j]; //multiply i and j part of array1 and array2 and put them into array3
                 }
             }
+           
+      
         }
     
-
+        
       
     }
 }
